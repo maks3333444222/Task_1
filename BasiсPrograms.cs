7ee037bfe9271995378dd520fb213a4e3be171f9
maks3333444222 <63209205+maks3333444222@users.noreply.github.com>
2026-09-14 19:54:19 +0300

    Add files via upload

---
+++ BasiсPrograms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proekt5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите количество секунд : ");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+
+            int hours = seconds / 3600;
+            int ostatok = seconds - hours * 3600;
+            int minutes = ostatok / 60;
+            int seconds2 = ostatok % 60;
+
+            Console.WriteLine($"Итог: {hours}ч, {minutes}мин, {seconds2} сек");
+            Console.ReadLine();
+
+
+        }
+    }
+}
